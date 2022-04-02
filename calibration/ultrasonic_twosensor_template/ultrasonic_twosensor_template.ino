d229692e78c6f3acbbca7eca6e03910a50ce5e35
Marvin Pranajaya <65348889+pipipipi2002@users.noreply.github.com>
2022-04-02 13:00:10 +0800

    implemented template for reading in 2 ultrasonic sensors distance (polling)

---
+++ calibration/ultrasonic_twosensor_template/ultrasonic_twosensor_template.ino
@@ -0,0 +1,45 @@
+#define trigFront 7
+#define echoFront 8
+#define trigSide 12
+#define echoSide 13
+
+#define TIMEOUT 30000
+#define WAITING_TIME 1000
+
+#define SENSOR_M1 28.476
+#define SENSOR_M2 28.249
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(trigFront, OUTPUT);
+  pinMode(echoFront, INPUT);
+  pinMode(trigSide, OUTPUT);
+  pinMode(echoSide, INPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  double distance_front = US_distance(trigFront, echoFront, SENSOR_M1);
+  double distance_side = US_distance(trigSide, echoSide, SENSOR_M2);
+
+  Serial.print("Front: ");
+  Serial.println(distance_front);
+  Serial.print("Side: ");
+  Serial.println(distance_side);
+  
+
+  delay(WAITING_TIME);
+
+}
+
+double US_distance(int trig, int echo, float constant) {
+  digitalWrite(trig, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trig, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trig, LOW);
+  long duration = pulseIn(echo, HIGH, TIMEOUT); 
+  double distance = (duration / 2.0) / constant;
+  return distance;
+}
